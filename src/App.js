8fa2d961a766d2cfdc91fdf47ae539e5958eaed0
lexymcr <@maingard.alexa@gmail.com>
2022-01-06 13:49:04 +0100

    Initial Commit Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router"
 
-function App() {
+import { HomePage } from "./components/HomePage";
+import "./styles/main.css"
+
+//Modify:
+//<Routes>
+//  <Route path="" element={< />} />
+//  <Route path="tickets/summary" element={< />} />
+//</Routes>
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
   );
 }
-
-export default App;
